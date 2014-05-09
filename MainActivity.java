1222d605d4690f0a658e480161bc79f3d39c7401
David Schmitz <davidpschmitz@gmail.com>
2014-05-08 23:09:10 -0500

    This is my MainActivity.java, which includes a background bitmap instead of a drawable.

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,53 @@
-package com.example.drawsomestuff;
+package com.example.testdrawable;
+//import java.io.InputStream;
 
-import android.R.drawable;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+//import android.view.Menu;
+import android.view.View;
+//import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.view.View;
-import android.view.View.OnClickListener;
- 
+
 public class MainActivity extends Activity {
- 
-	Button button;
+	
+	private Bitmap BackgroundImage;
 	ImageView image;
- 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
- 
-		addListenerOnButton();
- 
-	}
- 
-	public void addListenerOnButton() {
- 
-		image = (ImageView) findViewById(R.id.imageView1);
- 
-		button = (Button) findViewById(R.id.btnChangeImage);
+	Button button;
+	
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    image = (ImageView) findViewById(R.id.image);
+    Resources res = getResources();	
+    BackgroundImage = BitmapFactory.decodeResource(res,
+            R.drawable.img0005);
+    //InputStream resource = getResources().openRawResource(R.drawable.img0005);
+    //Bitmap bitmap = BitmapFactory.decodeStream(resource);
+    //button.setBackground(new MyRoundCornerDrawable(bitmap));
+    addListenerOnButton();
+  }
+
+  public void addListenerOnButton() {
+	  
+		image = (ImageView) findViewById(R.id.image);
+
+		button = (Button) findViewById(R.id.button);
 		button.setOnClickListener(new OnClickListener() {
- 
+
 			@Override
 			public void onClick(View arg0) {
-				image.setImageResource(R.drawable.canopy);
+				image.setImageResource(R.drawable.img0002);
 			}
- 
+
 		});
- 
+
 	}
- 
 }
